f05d8945934d4dfadc6c8e79c80e6fe3260d8a3c
Kelley van Evert <hello@klve.nl>
2022-12-04 11:29:56 +0100

    Day 4 bonus :)

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -13,6 +13,10 @@ fn main() {
         "num containments found: {}",
         solve(parse_entry(&filecontents))
     );
+    println!(
+        "num overlaps found: {}",
+        solve_v2(parse_entry(&filecontents))
+    );
 }
 
 fn solve(assignment_pairs: Vec<AssignmentPair>) -> i32 {
@@ -32,11 +36,41 @@ fn contains((le, ri): AssignmentPair) -> bool {
     le.start <= ri.start && le.end >= ri.end
 }
 
+fn solve_v2(assignment_pairs: Vec<AssignmentPair>) -> i32 {
+    assignment_pairs
+        .iter()
+        .map(|&p| if overlap(p) { 1 } else { 0 })
+        .sum::<i32>()
+}
+
+fn overlap((le, ri): AssignmentPair) -> bool {
+    (ri.start <= le.start && le.start <= ri.end)
+        || (ri.start <= le.end && le.end <= ri.end)
+        || (le.start <= ri.start && ri.start <= le.end)
+        || (le.start <= ri.end && ri.end <= le.end)
+}
+
 #[test]
 pub fn test_solve() {
     assert_eq!(
         2,
         solve(parse_entry(
+            "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+"
+        ))
+    );
+}
+
+#[test]
+pub fn test_solve_v2() {
+    assert_eq!(
+        4,
+        solve_v2(parse_entry(
             "2-4,6-8
 2-3,4-5
 5-7,7-9
