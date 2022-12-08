613fa5ffcdae62dd5ccdd5dce40eba9ae5d27f55
Kelley van Evert <kelley@ijsfontein.nl>
2022-12-08 11:51:01 +0100

    Solved day 8 bonus .. with some iterator drama..

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,11 +1,13 @@
-use std::fs;
+use std::{cmp, fs};
 
 fn main() {
     let s = fs::read_to_string("./input.txt").unwrap();
-    println!("Number of trees visible: {}", solve(&s));
+    let (num_visible, highest_score) = solve(&s);
+    println!("Number of trees visible: {}", num_visible);
+    println!("Highest score: {}", highest_score);
 }
 
-fn solve(s: &str) -> usize {
+fn solve(s: &str) -> (usize, usize) {
     let tree_heights: Vec<Vec<usize>> = s
         .lines()
         .into_iter()
@@ -20,13 +22,20 @@ fn solve(s: &str) -> usize {
     let h = tree_heights.len();
 
     let mut num_visible = 0;
+    let mut highest_score = 0;
 
     for y in 0..h {
         for x in 0..w {
             let height = tree_heights[y][x];
 
-            // could be optimized later by changing the order of the checks depending on the position
+            let score = count_until((0..y).rev(), |y0| height > tree_heights[y0][x])
+                * count_until((y + 1)..h, |y0| height > tree_heights[y0][x])
+                * count_until((0..x).rev(), |x0| height > tree_heights[y][x0])
+                * count_until((x + 1)..w, |x0| height > tree_heights[y][x0]);
 
+            highest_score = cmp::max(highest_score, score);
+
+            // could be optimized later by changing the order of the checks depending on the position
             let visible = (0..y).all(|y0| tree_heights[y0][x] < height)
                 || ((y + 1)..h).all(|y0| tree_heights[y0][x] < height)
                 || (0..x).all(|x0| tree_heights[y][x0] < height)
@@ -38,7 +47,7 @@ fn solve(s: &str) -> usize {
         }
     }
 
-    num_visible
+    (num_visible, highest_score)
 }
 
 #[test]
@@ -49,5 +58,22 @@ fn test_solve() {
 33549
 35390";
 
-    assert_eq!(solve(s), 21);
+    assert_eq!(solve(s), (21, 8));
+}
+
+fn count_until<I, P>(r: I, mut p: P) -> usize
+where
+    I: Iterator<Item = usize>,
+    P: FnMut(usize) -> bool,
+{
+    let mut accum = 0;
+
+    for item in r {
+        accum += 1;
+        if !p(item) {
+            return accum;
+        }
+    }
+
+    accum
 }
