f5996e8f110ef860fc7db7b4b9c68ed51cf69538
Kelley van Evert <kelley@ijsfontein.nl>
2022-12-12 10:25:18 +0100

    Solved day 12 :)

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,13 +1,19 @@
-use std::str::FromStr;
+use std::{fs, str::FromStr};
 
 fn main() {
-    println!("Hello, world!");
+    let filecontents = fs::read_to_string("./input.txt").unwrap();
+    let state = filecontents.parse().unwrap();
+    println!("Min number of steps: {}", solve(&state));
 }
+
+type Pos = (usize, usize);
 
 #[derive(PartialEq, Debug)]
 struct State {
-    pos: (usize, usize),
-    dest: (usize, usize),
+    pos: Pos,
+    dest: Pos,
+    height: usize,
+    width: usize,
     map: Vec<Vec<usize>>,
 }
 
@@ -17,7 +23,7 @@ impl FromStr for State {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut pos = (0, 0);
         let mut dest = (0, 0);
-        let map = s
+        let map: Vec<Vec<usize>> = s
             .lines()
             .enumerate()
             .map(|(y, line)| {
@@ -38,12 +44,74 @@ impl FromStr for State {
             })
             .collect();
 
-        Ok(Self { pos, dest, map })
+        Ok(Self {
+            pos,
+            dest,
+            height: map.len(),
+            width: map[0].len(),
+            map,
+        })
     }
 }
 
+impl State {
+    fn height(&self, pos: Pos) -> usize {
+        self.map[pos.1][pos.0]
+    }
+
+    fn neighbors(&self, pos: Pos) -> Vec<Pos> {
+        vec![
+            (pos.0 as i32 - 1, pos.1 as i32),
+            (pos.0 as i32 + 1, pos.1 as i32),
+            (pos.0 as i32, pos.1 as i32 - 1),
+            (pos.0 as i32, pos.1 as i32 + 1),
+        ]
+        .into_iter()
+        .filter(|&(x, y)| {
+            x >= 0 && y >= 0 && (x as usize) < self.width && (y as usize) < self.height
+        })
+        .map(|p| (p.0 as usize, p.1 as usize))
+        .collect()
+    }
+}
+
+fn solve(s: &State) -> usize {
+    let mut reachability: Vec<Vec<Option<usize>>> = s
+        .map
+        .iter()
+        .map(|line| line.iter().map(|_| None).collect())
+        .collect();
+
+    reachability[s.pos.1][s.pos.0] = Some(0);
+
+    let mut todo = vec![s.pos];
+
+    while todo.len() > 0 {
+        let p = todo.pop().unwrap();
+        let s1 = reachability[p.1][p.0].unwrap();
+        for n in s.neighbors(p) {
+            if s.height(n) <= s.height(p) + 1 {
+                match reachability[n.1][n.0] {
+                    None => {
+                        reachability[n.1][n.0] = Some(s1 + 1);
+                        todo.push(n);
+                    }
+                    Some(s0) => {
+                        if s1 + 1 < s0 {
+                            reachability[n.1][n.0] = Some(s1 + 1);
+                            todo.push(n);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    reachability[s.dest.1][s.dest.0].unwrap()
+}
+
 #[test]
-fn test_parse() {
+fn test_all() {
     let s = "Sabqponm
 abcryxxl
 accszExk
@@ -60,7 +128,10 @@ abdefghi";
             vec![0, 2, 2, 19, 20, 21, 22, 9],
             vec![0, 1, 3, 4, 5, 6, 7, 8],
         ],
+        width: 8,
+        height: 5,
     };
 
-    assert_eq!(Ok(state), s.parse());
+    assert_eq!(state, s.parse().unwrap());
+    assert_eq!(31, solve(&state));
 }
