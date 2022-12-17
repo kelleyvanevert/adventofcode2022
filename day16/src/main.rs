34e121fde49ad21f2dfd39ad878c76b85f824628
Kelley van Evert <kelley@ijsfontein.nl>
2022-12-17 11:58:00 +0100

    Day 16 is a mess :O First part is completed, but hella inefficient, took forever to compute

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,13 +1,20 @@
 use regex::Regex;
-use std::{collections::HashMap, fs};
+use std::{cmp, collections::HashMap, fmt::Debug, fs, time::Instant};
 
 fn main() {
     let filecontents = fs::read_to_string("./input.txt").unwrap();
+
     let data = parse(&filecontents);
-    println!("Hello, world!");
+
+    let t0 = Instant::now();
+    let max = State::new().find_best(&data, "".to_string());
+    println!("Max: {}", max);
+    println!(" took: {:?}", t0.elapsed());
 }
 
-fn parse<'a>(s: &'a str) -> HashMap<&'a str, (usize, Vec<&'a str>)> {
+type Data<'a> = HashMap<&'a str, (usize, Vec<&'a str>)>;
+
+fn parse<'a>(s: &'a str) -> Data<'a> {
     let re =
         Regex::new(r"^Valve ([A-Z]+) has flow rate=([0-9]+); tunnels? leads? to valves? (.*)$")
             .unwrap();
@@ -28,8 +35,115 @@ fn parse<'a>(s: &'a str) -> HashMap<&'a str, (usize, Vec<&'a str>)> {
     data
 }
 
+struct State<'a> {
+    at: &'a str,
+    time_left: usize,
+    valves: HashMap<&'a str, Option<usize>>,
+    visited: HashMap<&'a str, usize>,
+    total: usize,
+}
+
+impl<'a> Debug for State<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!(
+            "State({} @ {} ({}) total {})",
+            self.at,
+            self.time_left,
+            self.valves
+                .iter()
+                .filter_map(|(&name, &open)| { open.map(|n| format!("{}: {}", name, n)) })
+                .collect::<Vec<String>>()
+                .join(","),
+            self.total
+        ))
+    }
+}
+
+impl<'a> State<'a> {
+    fn new() -> Self {
+        Self {
+            at: "AA",
+            time_left: 30,
+            valves: HashMap::new(),
+            visited: HashMap::from([("AA", 0)]),
+            total: 0,
+        }
+    }
+
+    fn open_valve(&self, data: &Data<'a>) -> State<'a> {
+        let mut visited = self.visited.clone();
+        let mut valves = self.valves.clone();
+
+        let pressure_to_be_released = data[self.at].0 * (self.time_left - 1);
+        let total = self.total + pressure_to_be_released;
+
+        valves.insert(self.at, Some(pressure_to_be_released));
+        visited.insert(self.at, total);
+
+        Self {
+            at: self.at,
+            time_left: self.time_left - 1,
+            valves,
+            visited,
+            total,
+        }
+    }
+
+    fn goto(&self, dest: &'a str) -> State<'a> {
+        let mut visited = self.visited.clone();
+        visited.insert(dest, self.total);
+
+        Self {
+            at: dest,
+            time_left: self.time_left - 1,
+            valves: self.valves.clone(),
+            visited,
+            total: self.total,
+        }
+    }
+
+    fn find_best(&self, data: &Data<'a>, depth: String) -> usize {
+        let subdepth = format!("  {}", depth);
+        let mut max = self.total;
+
+        // println!(
+        //     "{}At {} time {} total {}",
+        //     depth, self.at, self.time_left, self.total
+        // );
+
+        if self.time_left > 0 {
+            if !self.valves.contains_key(self.at) && data[self.at].0 > 0 {
+                // [x] only if total increases
+                // println!("{} -> open", depth);
+                max = cmp::max(max, self.open_valve(data).find_best(data, subdepth.clone()));
+            }
+            for &dest in &data[self.at].1 {
+                // [ ] prevent unnecessary move back
+
+                //  AA 0 @ 30  -> DD 0 @ 29
+                // (DD 0 @ 29) -> AA 0 @ 28 <- prevent
+
+                match self.visited.get(dest) {
+                    None => {
+                        // not visited before -> defo try
+                        max = cmp::max(max, self.goto(dest).find_best(data, subdepth.clone()));
+                    }
+                    Some(&total) => {
+                        // only try if
+                        if total < self.total {
+                            max = cmp::max(max, self.goto(dest).find_best(data, subdepth.clone()));
+                        }
+                    }
+                }
+            }
+        }
+
+        max
+    }
+}
+
 #[test]
-fn test_parse() {
+fn test_all() {
     let s = "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
 Valve BB has flow rate=13; tunnels lead to valves CC, AA
 Valve CC has flow rate=2; tunnels lead to valves DD, BB
@@ -58,4 +172,6 @@ Valve JJ has flow rate=21; tunnel leads to valve II";
             ("JJ", (21, vec!["II"])),
         ])
     );
+
+    assert_eq!(State::new().find_best(&data, "".to_string()), 1651);
 }
