329b3b069240125c14dda822327c46a9bdc087bb
Kelley van Evert <kelley@ijsfontein.nl>
2022-12-22 22:43:37 +0100

    Holy guacamoly :P Ugly ugly code, but, I solved it!

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -15,13 +15,13 @@ fn main() {
     let filecontents = fs::read_to_string("./input.txt").unwrap();
 
     time(|| {
-        let pass = solve(&filecontents, None, false);
+        let pass = solve(&filecontents, None);
         println!("\nPassword: {}", pass);
         assert_eq!(pass, 149138);
     });
 
     time(|| {
-        let pass_3d = solve(&filecontents, Some(50), false);
+        let pass_3d = solve(&filecontents, Some(50));
         println!("\nPassword (cube): {}", pass_3d);
         assert_eq!(pass_3d, 153203);
     });
@@ -43,8 +43,8 @@ type Dir = usize;
 
 type Pos3D = (i32, i32, i32);
 
-// (original, rotated)
-type Face = ([Pos3D; 4], [Pos3D; 4]);
+// (original, transformed)
+type Face = ([Pos; 4], [Pos3D; 4]);
 
 fn add(a: Pos3D, b: Pos3D) -> Pos3D {
     (a.0 + b.0, a.1 + b.1, a.2 + b.2)
@@ -54,7 +54,11 @@ fn sub(a: Pos3D, b: Pos3D) -> Pos3D {
     (a.0 - b.0, a.1 - b.1, a.2 - b.2)
 }
 
-fn delta_step(b: Pos3D, a: Pos3D) -> Pos3D {
+fn delta_step(b: Pos, a: Pos) -> Pos {
+    ((b.0 - a.0).signum(), (b.1 - a.1).signum())
+}
+
+fn delta_step_3(b: Pos3D, a: Pos3D) -> Pos3D {
     (
         (b.0 - a.0).signum(),
         (b.1 - a.1).signum(),
@@ -64,11 +68,15 @@ fn delta_step(b: Pos3D, a: Pos3D) -> Pos3D {
 
 const DELTAS: [Pos; 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
 
+fn delta_to_dir(p: Pos) -> Dir {
+    DELTAS.iter().position(|&d| d == p).unwrap()
+}
+
 fn opposite(dir: Dir) -> Dir {
     (dir + 2) % 4
 }
 
-fn solve(s: &str, cube_side_length: Option<usize>, is_test_case: bool) -> i32 {
+fn solve(s: &str, cube_side_length: Option<usize>) -> i32 {
     let (map, path) = s.split_once("\n\n").unwrap();
     let mut path = path.trim();
 
@@ -85,7 +93,7 @@ fn solve(s: &str, cube_side_length: Option<usize>, is_test_case: bool) -> i32 {
 
     let height = grid.len();
 
-    // inclusive
+    // for part 1 only; inclusive
     let hbounds = (0..height)
         .map(|y| {
             let xmin = (0..width).find(|&x| grid[y][x] != ' ').unwrap_or(0);
@@ -94,7 +102,7 @@ fn solve(s: &str, cube_side_length: Option<usize>, is_test_case: bool) -> i32 {
         })
         .collect::<Vec<(i32, i32)>>();
 
-    // inclusive
+    // for part 1 only; inclusive
     let vbounds = (0..width)
         .map(|x| {
             let ymin = (0..height).find(|&y| grid[y][x] != ' ').unwrap_or(0);
@@ -103,6 +111,7 @@ fn solve(s: &str, cube_side_length: Option<usize>, is_test_case: bool) -> i32 {
         })
         .collect::<Vec<(i32, i32)>>();
 
+    // for part 2 only
     let teleports = if let Some(side_length) = cube_side_length {
         let cube = find_cube_folding({
             let mut faces = vec![];
@@ -110,14 +119,22 @@ fn solve(s: &str, cube_side_length: Option<usize>, is_test_case: bool) -> i32 {
             for x in (0..(width as i32)).step_by(side_length) {
                 for y in (0..(height as i32)).step_by(side_length) {
                     if grid[y as usize][x as usize] != ' ' {
-                        let face = [
-                            (x, y, 0),
-                            (x + side_length as i32, y, 0),
-                            (x + side_length as i32, y + side_length as i32, 0),
-                            (x, y + side_length as i32, 0),
-                        ];
-                        // println!("Face: {:?}", face);
-                        faces.push((face, face));
+                        faces.push((
+                            // "original"
+                            [
+                                (x, y),
+                                (x + side_length as i32 - 1, y),
+                                (x + side_length as i32 - 1, y + side_length as i32 - 1),
+                                (x, y + side_length as i32 - 1),
+                            ],
+                            // "mathematical"
+                            [
+                                (x, y, 0),
+                                (x + side_length as i32, y, 0),
+                                (x + side_length as i32, y + side_length as i32, 0),
+                                (x, y + side_length as i32, 0),
+                            ],
+                        ));
                     }
                 }
             }
@@ -125,39 +142,10 @@ fn solve(s: &str, cube_side_length: Option<usize>, is_test_case: bool) -> i32 {
             faces
         });
 
-        Some(compute_teleportation(&cube))
+        Some(compute_teleportation(&grid, &cube))
     } else {
         None
     };
-
-    // let mut teleport = HashMap::new();
-    // if is_test_case {
-    //     for i in 0..4 {
-    //         teleport.insert(((12 + i, 8), 3), ((11, 7 - i), 2)); // A
-    //         teleport.insert(((11, i), 0), ((15, 11 - i), 2)); // B
-    //         teleport.insert(((0, 4 + i), 2), ((15 - i, 11), 3)); // C
-    //         teleport.insert(((4 + i, 4), 3), ((8, i), 0)); // D
-    //         teleport.insert(((8 + i, 0), 3), ((3 - i, 4), 1)); // E
-    //         teleport.insert(((4 + i, 7), 1), ((8, 11 - i), 0)); // F
-    //         teleport.insert(((i, 7), 1), ((11 - i, 11), 3)); // G
-    //     }
-    // } else {
-    //     for i in 0..50 {
-    //         teleport.insert(((50 + i, 0), 3), ((0, 150 + i), 0)); // A
-    //         teleport.insert(((100 + i, 0), 3), ((i, 199), 3)); // B
-    //         teleport.insert(((50, i), 2), ((0, 149 - i), 0)); // C
-    //         teleport.insert(((50, 50 + i), 2), ((i, 100), 1)); // D
-    //         teleport.insert(((100 + i, 49), 1), ((99, 50 + i), 2)); // E
-    //         teleport.insert(((149, i), 0), ((99, 149 - i), 2)); // F
-    //         teleport.insert(((50 + i, 149), 1), ((49, 150 + i), 2)); // G
-    //     }
-    // }
-    // for ((source_pos, source_dir), (target_pos, target_dir)) in teleport.clone() {
-    //     teleport.insert(
-    //         (target_pos, opposite(target_dir)),
-    //         (source_pos, opposite(source_dir)),
-    //     );
-    // }
 
     let mut pos = (
         (0..width).position(|x| grid[1][x] == '.').unwrap() as i32,
@@ -185,21 +173,20 @@ fn solve(s: &str, cube_side_length: Option<usize>, is_test_case: bool) -> i32 {
                     let mut new_dir = dir;
 
                     if let Some(teleports) = &teleports {
-                        if grid[new_pos.1 as usize][new_pos.0 as usize] == ' ' {
-                            if let Some(target) = teleports.get(&new_pos) {
-                                println!(
-                                    "Teleport! from {:?} going {}, to {:?}",
-                                    new_pos, dir, target
-                                );
-                            }
+                        // part 2
+                        if new_pos.0 < 0
+                            || new_pos.1 < 0
+                            || new_pos.0 >= width as i32
+                            || new_pos.1 >= height as i32
+                            || grid[new_pos.1 as usize][new_pos.0 as usize] == ' '
+                        {
+                            (new_pos, new_dir) = teleports
+                                .get(&(pos, dir))
+                                .map(|p| (p.0, p.1))
+                                .expect("No teleport found");
                         }
-                        //
-                        //
-                        // if let Some((teleport_pos, teleport_dir)) = teleport.get(&(pos, dir)) {
-                        //     new_pos = *teleport_pos;
-                        //     new_dir = *teleport_dir;
-                        // }
                     } else {
+                        // part 1
                         if dir == 0 && new_pos.0 > hbounds[new_pos.1 as usize].1 {
                             new_pos = (hbounds[new_pos.1 as usize].0, new_pos.1);
                         } else if dir == 1 && new_pos.1 > vbounds[new_pos.0 as usize].1 {
@@ -262,7 +249,7 @@ fn num_points(v: &Vec<Face>) -> usize {
 fn adjacent(
     (a_orig, a): Face,
     (b_orig, b): Face,
-) -> Option<((Pos3D, Pos3D), Pos3D, (Pos3D, Pos3D), Pos3D, Pos3D)> {
+) -> Option<((Pos, Pos), Pos3D, (Pos, Pos), Pos3D, Pos3D)> {
     let edge = HashSet::from(a)
         .intersection(&HashSet::from(b))
         .map(|&p| p)
@@ -281,7 +268,7 @@ fn adjacent(
             start,
             (a_orig[a_end_i], b_orig[b_end_i]),
             end,
-            delta_step(end, start),
+            delta_step_3(end, start),
         ));
     }
 
@@ -378,105 +365,70 @@ fn find_cube_folding(faces: Vec<Face>) -> Vec<Face> {
         }
     }
 
-    unreachable!();
+    unreachable!("Could not find folding");
 }
 
-fn compute_teleportation(faces: &Vec<Face>) -> HashMap<Pos, Pos> {
+fn compute_teleportation(
+    grid: &Vec<Vec<char>>,
+    faces: &Vec<Face>,
+) -> HashMap<(Pos, Dir), (Pos, Dir)> {
     let mut teleports = HashMap::new();
 
-    for ((a_orig_start, b_orig_start), _, (a_orig_end, b_orig_end), _, _) in faces
+    let get_out_dir = |start: Pos, end: Pos| match delta_to_dir(delta_step(end, start)) {
+        0 | 2 => {
+            if start.1 <= 0 || grid[start.1 as usize - 1][start.0 as usize] == ' ' {
+                3
+            } else {
+                1
+            }
+        }
+        1 | 3 => {
+            if start.0 <= 0 || grid[start.1 as usize][start.0 as usize - 1] == ' ' {
+                2
+            } else {
+                0
+            }
+        }
+        _ => unreachable!(),
+    };
+
+    for ((a_start, b_start), _, (a_end, b_end), _, _) in faces
         .iter()
         .tuple_combinations()
         .filter_map(|(&a, &b)| adjacent(a, b))
     {
-        // println!(
-        //     "Map {:?} --- {:?} TO {:?} --- {:?}",
-        //     a_orig_start, a_orig_end, b_orig_start, b_orig_end
-        // );
+        if manhattan(a_start, b_start) <= 1 && manhattan(a_end, b_end) <= 1 {
+            continue;
+        }
 
-        for (&a, b) in steps(a_orig_start, a_orig_end)
-            .iter()
-            .zip(steps(b_orig_start, b_orig_end))
-        {
-            // println!("  teleport {:?} to {:?}", a, b);
-            teleports.insert((a.0, a.1), (b.0, b.1));
-            teleports.insert((b.0, b.1), (a.0, a.1));
+        let a_out = get_out_dir(a_start, a_end);
+        let b_out = get_out_dir(b_start, b_end);
+
+        for (&a, b) in steps(a_start, a_end).iter().zip(steps(b_start, b_end)) {
+            teleports.insert(((a.0, a.1), a_out), ((b.0, b.1), opposite(b_out)));
+            teleports.insert(((b.0, b.1), b_out), ((a.0, a.1), opposite(a_out)));
         }
     }
 
     teleports
 }
 
-fn steps(a: Pos3D, b: Pos3D) -> Vec<Pos3D> {
+fn steps(a: Pos, b: Pos) -> Vec<Pos> {
     let d = delta_step(b, a);
 
-    let mut steps = vec![];
-
     let mut curr = a;
+    let mut steps = vec![curr];
     while curr != b {
+        curr = (curr.0 + d.0, curr.1 + d.1);
         steps.push(curr);
-        curr = add(curr, d);
     }
 
     steps
 }
 
-// // inclusive on the low side, exclusive on the high side
-// // using the fact that only one axis will actually step
-// fn steps(mut a: Pos3D, mut b: Pos3D, side_length: i32) -> Vec<Pos3D> {
-//     let d = delta_step(b, a);
-//     assert!(!(d.0 != 0 && d.1 != 0 && d.2 != 0));
-
-//     let going_up = d.0 > 0 || d.1 > 0 || d.2 > 0;
-
-//     if d.0 == 0 && a.0 % side_length == 0 {
-//         a.0 -= 1;
-//         b.0 -= 1;
-//     }
-//     if d.1 == 0 && a.1 % side_length == 0 {
-//         a.1 -= 1;
-//         b.1 -= 1;
-//     }
-//     if d.2 == 0 && a.2 % side_length == 0 {
-//         a.2 -= 1;
-//         b.2 -= 1;
-//     }
-
-//     let mut steps = vec![];
-//     if going_up {
-//         // inclusive start
-//         steps.push(a);
-//     }
-
-//     let mut curr = a;
-//     loop {
-//         curr = add(curr, d);
-
-//         if curr == b {
-//             if !going_up {
-//                 // inclusive end
-//                 steps.push(curr);
-//             }
-//             break;
-//         } else {
-//             steps.push(curr);
-//         }
-//     }
-
-//     steps
-// }
-
-// #[test]
-// fn test_steps() {
-//     assert_eq!(
-//         steps((0, 4, 0), (4, 4, 0), 4),
-//         vec![(0, 3, 0), (1, 3, 0), (2, 3, 0), (3, 3, 0)]
-//     );
-//     // assert_eq!(
-//     //     steps((12, 4, 0), (8, 4, 0), 4),
-//     //     vec![(11, 4, 0), (10, 4, 0), (9, 4, 0), (8, 4, 0)]
-//     // );
-// }
+fn manhattan(a: Pos, b: Pos) -> i32 {
+    (b.0 - a.0).abs() + (b.1 - a.1).abs()
+}
 
 #[test]
 fn test_all() {
@@ -496,9 +448,7 @@ fn test_all() {
 10R5L5R10L4R5L5
 ";
 
-    assert_eq!(solve(s, None, true), 6032);
+    assert_eq!(solve(s, None), 6032);
 
-    assert_eq!(solve(s, Some(4), true), 5031);
-
-    // assert!(false);
+    assert_eq!(solve(s, Some(4)), 5031);
 }
